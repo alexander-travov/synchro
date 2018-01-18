a5892f6397fe90ce3f2d2f621af763f4c420b038
Alexander Travov <alexander.travov@gmail.com>
2018-01-19 00:36:46 +0300

    Barrier problem

---
+++ barrier.py
@@ -0,0 +1,39 @@
+# Barrier problem
+
+import sys
+from threading import Thread, Semaphore
+
+
+NUM_THREADS = 5
+COUNT = 0 # Number of threads arrived at barrier
+
+MUTEX = Semaphore(1)
+BARRIER = Semaphore(0)
+
+
+def run(n):
+    global COUNT
+    global NUM_THREADS
+
+    sys.stdout.write('before barrier {}\n'.format(n))
+
+    MUTEX.acquire()
+    COUNT += 1
+    if COUNT == NUM_THREADS:
+        for _ in range(NUM_THREADS):
+            BARRIER.release()
+    MUTEX.release()
+
+    # critical point
+    BARRIER.acquire()
+
+    sys.stdout.write('after barrier {}\n'.format(n))
+
+
+THREADS = [Thread(target=run, args=(i,)) for i in range(NUM_THREADS)]
+
+for t in THREADS:
+    t.start()
+
+for t in THREADS:
+    t.join()
