b8cd85249ca6a4296260452244c4d54e9428d031
Alexander Travov <alexander.travov@gmail.com>
2018-01-23 00:59:36 +0300

    FIFO barbershop

--- barbershop.py
+++ barbershop.py
@@ -6,6 +6,7 @@ from watcher import watch
 
 
 NUM_SEATS = 4
+QUEUE = []
 MUTEX = Semaphore(1)
 
 BARBER = Semaphore(0)
@@ -17,20 +18,21 @@ BARBER_DONE = Semaphore(0)
 
 def client(n):
     global NUM_SEATS
+    global QUEUE
     MUTEX.acquire()
-    if NUM_SEATS == 0:
+    if len(QUEUE) == NUM_SEATS:
         # go away
         sys.stdout.write("Client {}: No seats. Going away.\n".format(n))
         MUTEX.release()
     else:
-        NUM_SEATS -= 1 # take a seat
+        # take a seat
+        s = Semaphore(0)
+        QUEUE.append(s)
         MUTEX.release()
         sys.stdout.write("Client {}: Sitting.\n".format(n))
         BARBER.release() # signaling barber
+        s.acquire()
         BARBER_SEAT.acquire() # waiting for him to get of his chair
-        MUTEX.acquire()
-        NUM_SEATS += 1 # leave the waiting room
-        MUTEX.release()
         # get haircut
         sys.stdout.write("Client {}: Getting haircut.\n".format(n))
         CLIENT_DONE.release()
@@ -38,8 +40,13 @@ def client(n):
 
 
 def barber():
+    global QUEUE
     while True:
         BARBER.acquire()
+        MUTEX.acquire()
+        s = QUEUE.pop(0)
+        MUTEX.release()
+        s.release()
         BARBER_SEAT.release()
         # make haircut
         time.sleep(2 * random.random())
