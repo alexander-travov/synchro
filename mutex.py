09a074df7ec96312ca30f6603cfe1fb324bdf219
Alexander Travov <alexander.travov@gmail.com>
2018-01-23 19:24:42 +0300

    Use utils in mutex

--- mutex.py
+++ mutex.py
@@ -1,7 +1,7 @@
 # Incrementing counter N concurrently using mutex.
 
 from __future__ import print_function
-from threading import Thread, Semaphore
+from sync_utils import Thread, Semaphore
 
 
 N = 0
@@ -13,17 +13,14 @@ def inc():
     global N
     global S
     for _ in range(MAX):
-        # critical section
         MUTEX.acquire()
+        # critical section
         N += 1
         MUTEX.release()
 
 
 NUM_THREADS = 10
-THREADS = [Thread(target=inc) for _ in range(NUM_THREADS)]
-
-for t in THREADS:
-    t.start()
+THREADS = [Thread(inc) for _ in range(NUM_THREADS)]
 
 for t in THREADS:
     t.join()
