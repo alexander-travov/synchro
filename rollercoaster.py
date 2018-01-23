29e54f0d8bcbdf8f9fa7ebe0c2a23b7c3c41afbc
Alexander Travov <alexander.travov@gmail.com>
2018-01-23 16:51:50 +0300

    Sync utils module

--- rollercoaster.py
+++ rollercoaster.py
@@ -1,65 +1,60 @@
 import sys
 import time
-from threading import Thread, Semaphore
-from watcher import watch
-from reusable_barrier import Barrier
+from sync_utils import *
 
 
 NUM_PASSENGERS = 0
 CAR_CAPACITY = 10
 MUTEX = Semaphore(1)
-CAR = Semaphore(0)
 BOARD_QUEUE = Semaphore(0)
 UNBOARD_QUEUE = Semaphore(0)
 BARRIER = Barrier(CAR_CAPACITY)
 ALL_ON_BOARD = Semaphore(0)
 ALL_OFF_BOARD = Semaphore(0)
 
+
 def passenger():
     global NUM_PASSENGERS
+    leader = False
+    BOARD_QUEUE.wait()
     MUTEX.acquire()
     NUM_PASSENGERS += 1
-    sys.stdout.write("Passenger arrives.\n")
-    leader = False
+    sys.stdout.write("Passenger boards.\n")
     if NUM_PASSENGERS == CAR_CAPACITY:
         leader = True
-        CAR.release()
     else:
         MUTEX.release()
-    BOARD_QUEUE.acquire()
-    sys.stdout.write("Passenger boards.\n")
     BARRIER.wait()
     if leader:
-        ALL_ON_BOARD.release()
+        ALL_ON_BOARD.signal()
         sys.stdout.write("All onboard.\n")
-    UNBOARD_QUEUE.acquire()
+    UNBOARD_QUEUE.wait()
     sys.stdout.write("Passenger unboards.\n")
     BARRIER.wait()
     if leader:
-        ALL_OFF_BOARD.release()
-        sys.stdout.write("All offboard.\n")
         NUM_PASSENGERS = 0
+        sys.stdout.write("All offboard.\n")
+        ALL_OFF_BOARD.signal()
         MUTEX.release()
 
 
 def car():
     while True:
-        CAR.acquire()
-        sys.stdout.write("Car loads.\n")
+        sys.stdout.write("Car starts loading.\n")
         for _ in range(CAR_CAPACITY):
-            BOARD_QUEUE.release()
-        ALL_ON_BOARD.acquire()
+            BOARD_QUEUE.signal()
+        ALL_ON_BOARD.wait()
         sys.stdout.write("Car runs.\n")
-        sys.stdout.write("Car unloads.\n")
+        sys.stdout.write("Car starts unloading.\n")
         for _ in range(CAR_CAPACITY):
-            UNBOARD_QUEUE.release()
-        ALL_OFF_BOARD.acquire()
+            UNBOARD_QUEUE.signal()
+        ALL_OFF_BOARD.wait()
 
 
 def main():
-    Thread(target=car).start()
+    Thread(car)
     while True:
         time.sleep(0.1)
-        Thread(target=passenger).start()
+        Thread(passenger)
 
 watch(main)
---
+++ sync_utils.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import signal
+import threading
+
+
+if sys.version_info.major == 2:
+    _Semaphore = threading._Semaphore
+else:
+    _Semaphore = threading.Semaphore
+
+
+class Semaphore(_Semaphore):
+    wait = _Semaphore.acquire
+    signal = _Semaphore.release
+
+
+class Thread(threading.Thread):
+    def __init__(self, target, *args):
+        threading.Thread.__init__(self, target=target, args=args)
+        self.start()
+
+
+class Barrier:
+    def __init__(self, num_threads):
+        self.num_threads = num_threads
+        self.count = 0
+        self.mutex = Semaphore(1)
+        self.turnstiles = (Semaphore(0), Semaphore(0))
+        self.t = 0 # current active turnstile index
+
+    def wait(self):
+        turnstile = self.turnstiles[self.t]
+
+        self.mutex.acquire()
+        self.count += 1
+        if self.count == self.num_threads:
+            self.count = 0
+            self.t = 1 - self.t # alternate turnstile index
+            for _ in range(self.num_threads):
+                turnstile.release()
+        self.mutex.release()
+
+        turnstile.acquire()
+
+
+def watch(run, *args, **kwargs):
+    try:
+        run(*args, **kwargs)
+        signal.pause()
+    except KeyboardInterrupt:
+        os.kill(os.getpid(), signal.SIGTERM)
